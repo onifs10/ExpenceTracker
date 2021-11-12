a5df145debe648cbc36b79abcb4bb8ca77cc9c04
onifs10 <onifadebolu64@gmail.com>
2021-11-12 13:56:48 +0100

    udpate;

--- src/utils/protectedRouteHandler.util.ts
+++ src/utils/protectedRouteHandler.util.ts
@@ -3,10 +3,13 @@ import passport from "passport";
 
 type HandleRequest = (req: Request, res: Response, next: NextFunction) => void;
 
+// handle protected request middleware
 const HandleProtectedRequest: HandleRequest = (req, res, next) => {
-  passport.authenticate("jwt", { session: false })(req, res, () => {
-    next();
+  //  calling the passport auth function
+  const authenticationFunction = passport.authenticate("jwt", {
+    session: false,
   });
+  authenticationFunction(req, res, next);
 };
 
 export default HandleProtectedRequest;
